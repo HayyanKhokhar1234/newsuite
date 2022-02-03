a296d2c10c77ecd8788fc2bad4b15d75fb646b15
HayyanKhokhar1234 <haykho0809@highgateschool.org.uk>
2022-02-03 20:05:14 +0000

    ok

--- vernier cipher.py
+++ vernier cipher.py
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     print("Encrypted message:", encrypt_text)
     print("Decrypted message:", decryption(encrypt_text, key))
 
-print('Look at this commit lol hi.')
+print('Look at this commit lol hi loe.')
