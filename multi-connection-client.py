ef4e58419335c9a88cafc133e4644b93155f8314
HayyanKhokhar1234 <haykho0809@highgateschool.org.uk>
2022-03-07 22:13:06 +0000

    Port number was not the same so message did not echo, now it works

--- multi-connection-client.py
+++ multi-connection-client.py
@@ -1,6 +1,7 @@
 import selectors
 import socket
 import types
+import sys
 
 sel=selectors.DefaultSelector()
 def accept_wrapper(sock):
@@ -20,52 +21,55 @@ def start_connections(host, port, num_conns):
     server_addr = (host, port)
     for i in range(0, num_conns):
         connid = i + 1
-        print('starting connection', connid, 'to', server_addr)
+        print(f"Starting connection {connid} to {server_addr}")
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.setblocking(False)
         sock.connect_ex(server_addr)
         events = selectors.EVENT_READ | selectors.EVENT_WRITE
-        data = types.SimpleNamespace(connid=connid,
-                                     msg_total=sum(len(m) for m in messages),
-                                     recv_total=0,
-                                     messages=messages.copy(),
-                                     outb=b'',
-                                     )
+        data = types.SimpleNamespace(
+            connid=connid,
+            msg_total=sum(len(m) for m in messages),
+            recv_total=0,
+            messages=messages.copy(),
+            outb=b"",
+        )
         sel.register(sock, events, data=data)
 
 
-def service_connection(key,mask):
-    sock=key.fileobj
-    data=key.data
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
     if mask & selectors.EVENT_READ:
-        recv_data = sock.recv(1024) #should be ready to read
+        recv_data = sock.recv(1024)  # Should be ready to read
         if recv_data:
-            print(f"Recieved {recv_data!r} from connection {data.connid}")
-            data.recv_total+= len(recv_data)
+            print(f"Received {recv_data!r} from connection {data.connid}")
+            data.recv_total += len(recv_data)
         if not recv_data or data.recv_total == data.msg_total:
-            print('closing connection', data.connid)
+            print(f"Closing connection {data.connid}")
             sel.unregister(sock)
             sock.close()
-        if mask & selectors.EVENT_WRITE:
-            if not data.outb and data.messages:
-                data.outb= data.messages.pop(0)
-            if data.outb:
-                print(f"Sending {data.outb!r} to connection {data.connid}")
-                sent=sock.send(data.outb) #should be ready to write
-                data.outb=data.outb[sent:]
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages:
+            data.outb = data.messages.pop(0)
+        if data.outb:
+            print(f"Sending {data.outb!r} to connection {data.connid}")
+            sent = sock.send(data.outb)  # Should be ready to write
+            data.outb = data.outb[sent:]
 
 HOST='127.0.0.1' #Standard loopback interface address (local host)
-PORT= 65432
+PORT= 17000
 start_connections(HOST,PORT,2)
 
 try:
     while True:
-        events = sel.select(timeout=None)
-        for key, mask in events:
-            if key.data is None:
-                accept_wrapper(key.fileobj)
-            else:
-                service_connection(key, mask)
+        events = sel.select(timeout=1)
+        if events:
+            for key, mask in events:
+                    service_connection(key, mask)
+                    print('Lol')
+        if not sel.get_map():
+            break
+
 except KeyboardInterrupt:
     print("Caught keyboard interrupt, exiting")
 finally:
