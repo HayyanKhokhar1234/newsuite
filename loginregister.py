a2c9b6da1e150d6801a689e7c5e5c7c0a58e7772
HayyanKhokhar1234 <haykho0809@highgateschool.org.uk>
2022-03-17 16:49:41 +0000

    Updated GUI of login register page.

--- loginregister.py
+++ loginregister.py
@@ -1,29 +1,6 @@
 from tkinter import *
 from functools import partial
 
-def main_account_screen():
-    global main_screen
-    main_screen = Tk()
-    main_Screen.geometry('300x250')
-    main_screen.title('Account Login')
-
-
-
-#Create form label
-Label(text='Choose Log in or Register', bg='blue', width='300', height='2', font= ('Calibri',13)).pack()
-Label(text='').pack()
-
-#Create Log in button
-Button(text='Login', height='2',width='30').pack()
-Label(text='').pack()
-
-#Create a register button
-Button(text='Register', height='2', width='30').pack()
-Label(text='').pack()
-
-main_screen.mainloop()
-
-
 
 
 
@@ -51,9 +28,30 @@ def register():
     password_lable = Label(register_screen, text= 'Password * ')
     password_lable.pack()
 
-    password_entry = Entry(register_screen, textvariable=password, show='*')
-    passord_entry.pack()
+    password_entry = Entry(register_screen, textvariable=password,show='*')
+    password_entry.pack()
 
     Label(register_screen, text= '').pack()
 
     Button(register_screen, text= 'Register', width=10, height=1, bg='blue').pack()
+def main_account_screen():
+    global main_screen
+    main_screen = Tk()
+
+    main_screen.geometry('250x350')
+    main_screen.title('Account Login')
+    Label(text='Choose Log in or Register', bg='blue', width='300', height='2', font= ('Calibri',13)).pack()
+    Label(text='').pack()
+
+#Create Log in button
+    Button(text='Login', height='2',width='30').pack()
+    Label(text='').pack()
+
+#Create a register button
+    Button(text='Register', height='2', width='30',command=register).pack()
+    Label(text='').pack()
+
+
+
+main_account_screen()
+main_screen.mainloop()
